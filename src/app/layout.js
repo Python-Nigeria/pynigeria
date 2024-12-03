994ef8bb6c21759ea3eaa78cd38488a0b06d2866
Aourl24 <awwalmustapha13@gmail.com>
2024-12-04 00:44:43 +0100

    'updated'

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Pyton Nigeria",
+  title: "Python Nigeria",
   description: "Python Nigeria Official Website",
 };
 
