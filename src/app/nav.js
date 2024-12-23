1a00e4a40b21d76363053983a722966507d1179d
Aourl24 <awwalmustapha13@gmail.com>
2024-12-23 16:51:16 +0100

    'updated'

--- src/app/nav.js
+++ src/app/nav.js
@@ -8,6 +8,7 @@ export default function Nav(){
 	const path = usePathname()
 	const excludedPath= ["/account/signup"]
 	const [isScrolled,setIsScrolled]= React.useState(false)
+	const [dropdown , showDrop] = React.useState(false)
 	const {scrollY} = useScroll()
 	const animateClass = `${isScrolled ? "color-bg-white color-black shadow-sm" : "color-bg-white color-black"}`
 
@@ -32,8 +33,12 @@ export default function Nav(){
 	return(
 		<nav id="nav" class={`container-fluid sz-14 sticky-top py-2 py-md-3 ${path === "/" ? animateClass : "color-bg-white color-black shadow-sm" } ease`} style={{zIndex:'20'}} >
         <div class="row align-items-center">
-          <div class="col-6 p-4 sz-16 bold">
-            <i class="fab fa-python color-s"></i> Python Nigeria
+          <div class="col col-md-6 p-4 sz-16 bold">
+          <div class="row align-items-center">
+          <div class="col-1 col-md-1  p-0">
+            <img src="/logo.svg" class="img-fluid" style={{height:'25px',width:'auto'}}/> </div>
+            <div class="col p-1">Python Nigeria</div>
+          </div>
           </div>
           <div class="col font-poppins p-4 right display-sm-none"> 
             Dashboard 
@@ -50,10 +55,26 @@ export default function Nav(){
           <div class="col font-poppins p-4 right display-sm-none">
             <Link class="color-bg-p color-white no-decoration p-2 rounded" href="/"> Join Us </Link>
           </div>
-          <div class="col d-md-none right sz-24">
-          	<i class="fas fa-bars"></i>
+          <div class="col-2 d-md-none right sz-20">
+          	{!dropdown && <span onClick={()=>showDrop(true)}> <i class="fas fa-ellipsis-v font-montserrat-bold color-p"></i><i class="fas fa-ellipsis-v font-montserrat-bold color-p"></i><i class="fas fa-ellipsis-v font-montserrat-bold color-p"></i> </span>}
           </div>
         </div>
+        {dropdown && <Dropdown close={()=>showDrop(false)} /> }
         </nav>
 		)
+}
+
+
+function Dropdown ({close}){
+	return(
+		<div class="container-fluid  border-top sz-18 vh-100 display-fixed ease">
+		<br />
+    <div class="py-3"><a class="dropdown-item" href="#">Dashboard</a></div>
+    <div class="py-3"><a class="dropdown-item" href="#">Jobs</a></div>
+    <div class="py-3"><a class="dropdown-item" href="#">Forums</a></div>
+    <div class="py-3"><a class="dropdown-item" href="#">Resorces</a></div>
+
+    <div class="my-2 mt-4"><button class="btn btn-danger w-100" onClick={()=>close()}> Close </button></div>
+		</div>
+	)
 }
