4f0a6e0dc9ce35cc64e0edc05abce81ca7a493c1
Aourl24 <awwalmustapha13@gmail.com>
2025-01-02 14:22:14 +0100

    'updated'

--- src/app/membership/component.js
+++ src/app/membership/component.js
@@ -1,6 +1,6 @@
 "use client"
 import React from 'react'
-import basic from '../members.json'
+// import basic from '../members.json'
 
 export default function App(){
 	const [verify,setVerify] = React.useState()
